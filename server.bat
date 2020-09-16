28ddfde53fe8a0c608686bce5aec4b584bc41d60
Nicolas Malgat <nicolas.malgat@hotmail.com>
2020-09-16 11:40:04 +0200

    fix: server.bat works now

--- server.bat
+++ server.bat
@@ -1,9 +1,9 @@
 @echo off
 SET FLASK_APP=app
 SET FLASK_ENV=development
-start cmd /k serve front_app/dist -l 5000
-start cmd /k python -m flask run -p 5001
-echo Serveurs en fonctionnement:
-echo flask en http://127.0.0.1:5001/
-echo vue en http://192.168.68.140:5000/
-pause
+start cmd /k "cd front_app && npm run serve"
+start cmd /k python -m flask run -p 5000
+echo Serveurs en fonctionnement
+echo flask en http://127.0.0.1:5000/
+echo vue en http://192.168.68.140:8080/
+pause Veuillez fermer les serveurs vous-même
