3cb17013a7ff6bc7cd64f9c12dafa9a7d3fb3152
Nicolas Malgat <nicolas.malgat@hotmail.com>
2020-09-16 11:43:05 +0200

    fix server.bat

--- server.bat
+++ server.bat
@@ -4,6 +4,6 @@ SET FLASK_ENV=development
 start cmd /k "cd front_app && npm run serve"
 start cmd /k python -m flask run -p 5000
 echo Serveurs en fonctionnement
-echo flask en http://127.0.0.1:5000/
-echo vue en http://192.168.68.140:8080/
-pause Veuillez fermer les serveurs vous-même
+echo Flask en http://127.0.0.1:5000/
+echo Vue en http://192.168.68.140:8080/
+pause "Veuillez fermer les serveurs vous-même"
