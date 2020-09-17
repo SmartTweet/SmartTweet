7c396aced60ca2346a0c6e22e394c7109bb80b0b
Nicolas Malgat <nicolas.malgat@hotmail.com>
2020-09-17 09:00:28 +0200

    add new libraries and components

---
+++ front_app/src/components/TweetList.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="TweetList">
+    <FilterBar v-on:filtrer="filtrerList" />
+    <List :list="listeFiltree" />
+</div>
+</template>
+
+<script>
+import FilterBar from "./FilterBar.vue";
+import List from "./List.vue";
+
+export default {
+
+    name: "TweetList",
+    components: {
+        FilterBar,
+        List
+    },
+    props: {
+        list: Array,
+    },
+    data() {
+        return {
+            listeFiltree: this.list
+        }
+    },
+    methods: {
+        filtrerList(filtre) {
+            console.log(filtre)
+            // mecanique de filtre
+            this.listeFiltree = this.list.filter(function () {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                }
+            }
+        }
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+.TweetList {
+    height: 20em;
+    width: 60%;
+    background-color: #424242;
+    margin: 3em;
+    padding: 1em;
+}
+
+li:hover {
+    cursor: pointer;
+    background-color: #424242;
+    color: ivory;
+}
+</style>
---
+++ front_app/src/components/TweetPanel.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="TweetPanel">
+    <TweetList :list=tweet_list />
+</div>
+</template>
+
+<script>
+import TweetList from "./TweetList.vue";
+
+export default {
+    name: "TweetPanel",
+    data() {
+        return {
+            tweet_list: [
+                "patate",
+                "tomate",
+                "pomme"
+            ]
+        }
+    },
+    components: {
+        TweetList,
+    },
+    methods: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+.TweetPanel {
+    width: 100%;
+    background-color: grey;
+}
+</style>
